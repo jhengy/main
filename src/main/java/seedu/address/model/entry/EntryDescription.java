68d14008708832f10d270d63aec1bfaa5bcf6ccf
jhengy <35480886+jhengy@users.noreply.github.com>
2018-10-08 23:39:36 +0800

    Add Entry related classes

---
+++ src/main/java/seedu/address/model/entry/EntryDescription.java
@@ -0,0 +1,44 @@
+package seedu.address.model.entry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
+
+/**
+ *  Represents bullet points content for an entry.
+ */
+public class EntryDescription {
+    private List<String> descriptionList;
+
+    public EntryDescription() {
+        this.descriptionList = new ArrayList<>();
+    }
+
+    public List<String> getDescriptionList() {
+        return descriptionList;
+    }
+
+    public void addBullet() {
+
+    }
+
+    public void editBullet(Index index, String editedMsg) {
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof EntryDescription
+                && descriptionList.equals(((EntryDescription) other)
+                    .getDescriptionList()));
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        descriptionList.forEach(s -> builder.append(s).append("\n"));
+        return builder.toString();
+    }
+}
